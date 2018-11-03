c0f71fc23a151131559e8480bd3984544dc2690a
VA-Chusovitin <tchsl98@yandex.ru>
2018-11-03 20:29:10 +0500

    4 задача

--- emitter.js
+++ emitter.js
@@ -11,6 +11,13 @@ const isStar = true;
  * @returns {Object}
  */
 function getEmitter() {
+    let events = {};
+
+    function announceEvent(context, handler, times = Infinity, frequency = 1) {
+        return { 'context': context, 'handler': handler, 'times': times,
+            'frequency': frequency, 'count': 0 };
+    }
+
     return {
 
         /**
@@ -18,26 +25,53 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!events[event]) {
+                events[event] = [];
+            }
+            events[event].push(announceEvent(context, handler));
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            for (let key in events) {
+                if (key === event || key.startsWith(event + '.')) {
+                    events[key] = events[key].filter(element => element.context !== context);
+                }
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            do {
+                if (event in events) {
+                    events[event].forEach(element => {
+                        if (element.times > 0 && element.count % element.frequency === 0) {
+                            element.handler.call(element.context);
+                        }
+                        element.times--;
+                        element.count++;
+                    });
+                }
+                event = event.substring(0, event.lastIndexOf('.'));
+            } while (event !== '');
+
+            return this;
         },
 
         /**
@@ -47,9 +81,15 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} this
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            if (events[event]) {
+                events[event] = [];
+            }
+            events[event].push(announceEvent(context, handler, times));
+
+            return this;
         },
 
         /**
@@ -59,9 +99,15 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} this
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            if (events[event]) {
+                events[event] = [];
+            }
+            events[event].push(announceEvent(context, handler, frequency));
+
+            return this;
         }
     };
 }
